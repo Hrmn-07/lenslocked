0cdd1f8533d4f72c877d062bb7aa7b32861d2c6b
hrmn07 <herman_mac@HRM-MACBOOK.attlocal.net>
2024-03-09 15:49:23 -0600

    modified static controller

--- controllers/static.go
+++ controllers/static.go
@@ -10,10 +10,6 @@ type Static struct {
 	Template views.Template
 }
 
-func (static Static) ServeHttp(w http.ResponseWriter, r *http.Request) {
-	static.Template.Execute(w, nil)
-}
-
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
