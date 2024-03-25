fb64f1aa2410fb03e00f6af1e788b9c5b50ebdf1
hrmn07 <herman_mac@HRM-MACBOOK.attlocal.net>
2024-03-25 00:15:36 -0500

    added some features to exp.go

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -58,13 +58,80 @@ func main() {
 	}
 	fmt.Println("Tables created.")
 
-	name := "Jon Calhoun"
-	email := "jon@calhoun.io"
-	_, err = db.Exec(`
-	INSERT INTO users(name, email)
-	VALUES($1, $2);`, name, email)
+	// (For adding a user)
+	// name := "New User"
+	// email := "jon@calhoun.io"
+	// row := db.QueryRow(`
+	// INSERT INTO users(name, email)
+	// VALUES($1, $2) RETURNING id;`, name, email)
+	// var id int
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("User created. id =", id)
+
+	// (For getting a user's info)
+	// id := 1
+	// row := db.QueryRow(
+	// 	`SELECT name, email
+	// 	FROM users
+	// 	WHERE id=$1;`, id,
+	// )
+	// var name, email string
+	// err = row.Scan(&name, &email)
+	// if err == sql.ErrNoRows {
+	// 	fmt.Println("Error: No Rows!")
+	// }
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("User Information: name=%s, email=%s\n", name, email)
+
+	// (For creating orders)
+	// userID := 1
+	// for i := 1; i <= 5; i++ {
+	// 	amount := i * 100
+	// 	desc := fmt.Sprintf("Fake order #%d", i)
+	// 	_, err := db.Exec(`
+	// 	INSERT INTO orders(user_id, amount, description)
+	// 	VALUES($1, $2, $3);`, userID, amount, desc)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+	// fmt.Println("Created fake orders.")
+
+	type Order struct {
+		ID          int
+		UserID      int
+		Amount      int
+		Description string
+	}
+	var orders []Order
+
+	userID := 1
+	rows, err := db.Query(`
+	SELECT id, amount, description
+	FROM orders
+	WHERE user_id=$1;`, userID)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("User created.")
+	defer rows.Close()
+
+	for rows.Next() {
+		var order Order
+		order.UserID = userID
+		err := rows.Scan(&order.ID, &order.Amount, &order.Description)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	err = rows.Err()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Orders:", orders)
 }
