fd1a2644410047ab5b956b798a71c10d470ce548
hrmn07 <herman_mac@HRM-MACBOOK.attlocal.net>
2024-03-25 01:10:19 -0500

    added password hashing and comparing to bcrypt.go at cmd

---
+++ cmd/bcrypt/bcrypt.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "hash":
+		hash(os.Args[2])
+
+	case "compare":
+		compare(os.Args[2], os.Args[3])
+	default:
+		fmt.Printf("invalid command %v\n", os.Args[1])
+	}
+}
+
+func hash(password string) {
+	hashedBytes, err := bcrypt.GenerateFromPassword(
+		[]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Printf("error hashing %v\n", password)
+		return
+	}
+	// convert the returned byte slice of numbers into a string
+	hash := string(hashedBytes)
+	fmt.Println(hash)
+}
+
+func compare(password, hash string) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		fmt.Printf("Invalid password: %v\n", password)
+	}
+	fmt.Println("Password is correct!")
+}
